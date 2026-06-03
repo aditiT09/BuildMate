4dc6ac54ad4e1266e4c3c3463624cca25be7e546
Aditi Tiwari <adititiwari095@gmail.com>
2026-06-03 20:32:56 +0530

    chore: configure frontend API URL via environment variables

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://buildmate-5abm.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
